8594fc9a303870d58cf8157d88d183605e0a5434
Timothy Viccari <tjvicc84@gmail.com>
2021-04-13 17:37:43 -0400

    completed 3 and 4

--- code-challenges/challenges-02.test.js
+++ code-challenges/challenges-02.test.js
@@ -36,6 +36,9 @@ Write a function named addQuestion that, given an array of strings, uses map to 
 
 const addQuestion = (arr) => {
   // Solution code here...
+  let huh = arr.map( n => n.concat('?'));
+
+  return huh;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -50,6 +53,9 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 
 const forLoopTwoToThe = (arr) => {
   // Solution code here...
+  let deuce = arr.map( n => Math.pow(2, n));
+
+  return deuce;
 };
 
 /* ------------------------------------------------------------------------------------------------
