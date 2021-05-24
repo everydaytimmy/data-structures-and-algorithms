fb7fc431ce5754a5012ded5402830ff93f2bf509
‘Timothy <Timothy.viccari@gmail.com>
2021-05-23 20:03:41 -0400

    code challenge 5

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,41 @@
 class LinkedList:
-    """
-    Put docstring here
-    """
+    def __init__(self, head=None):
+        self.head = head
 
-    def __init__(self):
-        # initialization here
-        pass
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, value):
+        self.head = Node(value, self.head)
+
+    def includes(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+
+        return False
+
+
+    def find_all(self):
+        values = []
+        current = self.head
+
+        while current:
+            values.append(current.value)
+            current = current.next
+
+        return values
+
+    def __str__(self):
+        string = ""
+        current = self.head
+        while current:
+            string.join(current.value)
+            current = current.next
+
+        return string
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,50 @@
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import LinkedList, Node
 
 
 def test_import():
     assert LinkedList
+
+
+def test_insert():
+    list = LinkedList(Node("Yo"))
+    assert list.head.value == "Yo"
+    list.insert('sup')
+    assert list.head.value == 'sup'
+
+def test_includes():
+    list = LinkedList()
+    list.insert('rubber')
+    list.insert('baby')
+    list.insert('buggy')
+    list.insert('bumpers')
+    actual = list.includes('baby')
+    expected = True
+    actual == expected
+
+def test_head():
+    node = Node('rubber')
+    actual = node.next
+    expected = None
+    assert actual == expected
+
+def test_to_string():
+    list = LinkedList()
+    list.insert("{ c } ->")
+    list.insert("{ b } ->")
+    list.insert("{ a } ->")
+    actual = list.__str__()
+    expected = "{ a } -> { b } -> { c } -> None"
+    assert actual == expected
+
+def test_all_values():
+    list = LinkedList()
+    list.insert('bumpers')
+    list.insert('buggy')
+    list.insert('baby')
+    list.insert('rubber')
+    actual = list.find_all()
+    expected = ['rubber', 'baby', 'buggy', 'bumpers']
+    assert actual == expected
+
+
+
