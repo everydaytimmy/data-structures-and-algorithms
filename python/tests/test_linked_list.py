3c3b9d112c52d9fa4a03386ddebf7f8bd7715ff9
‘Timothy <Timothy.viccari@gmail.com>
2021-05-26 18:46:41 -0400

    cc6 resubmit

--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -55,7 +55,7 @@ def test_to_string():
     expected = "{ a } -> { b } -> { c } -> None"
     assert actual == expected
 
-def test_all_values():
+def test_values():
     list = LinkedList()
     list.insert('bumpers')
     list.insert('buggy')
@@ -65,22 +65,35 @@ def test_all_values():
     expected = ['rubber', 'baby', 'buggy', 'bumpers']
     assert actual == expected
 
-def insert_before_value():
+def test_add_before_value():
     list = LinkedList()
     list.insert('bumpers')
-    list.insert('baby')
+    list.insert('buggy')
     list.insert('rubber')
-    actual = list.inject_b('bumpers', 'buggy')
+    list.inject_b('buggy', 'baby')
+    actual = list.find_all()
     expected = ['rubber', 'baby', 'buggy', 'bumpers']
     assert actual == expected
 
-def insert_after_value():
+def test_add_after_value():
     list = LinkedList()
     list.insert('bumpers')
     list.insert('baby')
     list.insert('rubber')
-    actual = list.inject_b('baby', 'buggy')
+    list.inject_a('baby', 'buggy')
+    actual = list.find_all()
     expected = ['rubber', 'baby', 'buggy', 'bumpers']
+    assert actual == expected
+
+def test_add_after_value1():
+    list = LinkedList()
+    list.append_item('rock')
+    list.append_item('paper')
+    list.append_item('scissors')
+    list.append_item('machinegun')
+    list.inject_a('scissors', 'cannon')
+    actual = list.find_all()
+    expected = ['rock', 'paper', 'scissors', 'cannon', 'machinegun']
     assert actual == expected
 
 
@@ -94,7 +107,7 @@ def test_x_fromend():
     expected = 'paper'
     assert actual == expected
 
-def test_x_fromend():
+def test_x_fromend10():
     list = LinkedList()
     list.append_item('rock')
     list.append_item('paper')
@@ -104,7 +117,7 @@ def test_x_fromend():
     expected = 'Exception'
     assert actual == expected
 
-def test_x_fromend():
+def test_x_fromend0():
     list = LinkedList()
     list.append_item('rock')
     list.append_item('paper')
@@ -114,17 +127,29 @@ def test_x_fromend():
     expected = 'machinegun'
     assert actual == expected
 
-def test_x_fromend():
+def test_x_fromend_one_item():
     list = LinkedList()
     list.append_item('machinegun')
     actual = list.x_fromend(0)
     expected = 'machinegun'
     assert actual == expected
 
-def test_x_fromend():
+def test_x_fromend_neg():
     list = LinkedList()
     list.append_item('machinegun')
     actual = list.x_fromend(-2)
     expected = 'Exception'
     assert actual == expected
+
+# def test_zip():
+#     lista = LinkedList()
+#     listb = LinkedList()
+#     lista.append_item('A')
+#     lista.append_item('B')
+#     listb.append_item('1')
+#     listb.append_item('2')
+#     answer = lista.zip_list(listb)
+#     actual = answer.findall()
+#     expected = ['A', '1', 'B', '2']
+#     assert actual == expected
 
