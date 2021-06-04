75cafdb25de936ec098bbcf70aeb97be372a1b66
‘Timothy <Timothy.viccari@gmail.com>
2021-06-03 20:06:00 -0400

    challenge complete

--- python/tests/test_animal_shelter.py
+++ python/tests/test_animal_shelter.py
@@ -1,16 +1,12 @@
 from code_challenges.fifo_animal_shelter.fifo_animal_shelter import Cat, Dog,  Queue, AnimalShelter
 
-def test_is_queue():
-    assert Queue()
 
-def test_enqueue_multiple_animals():
-    queue = Queue()
-    queue.enqueue('Cat')
-    queue.enqueue('Cat')
-    queue.enqueue('Dog')
-    queue.enqueue('Cat')
-    actual = queue.rear.value
-    expected = 'Cat'
+def test_single_animal():
+    shelter = AnimalShelter()
+    cat = Cat()
+    shelter.enqueue(cat)
+    actual = shelter.dequeue("cat")
+    expected = cat
     assert actual == expected
 
 def test_animal_dequeue():
@@ -22,4 +18,16 @@ def test_animal_dequeue():
     shelter.enqueue(dog)
     actual = shelter.dequeue('dog')
     expected = dog
+    assert actual == expected
+
+def test_animal_dequeue_none():
+    shelter = AnimalShelter()
+    cat = Cat()
+    dog = Dog()
+    shelter.enqueue(cat)
+    shelter.enqueue(dog)
+    shelter.enqueue(cat)
+    shelter.enqueue(dog)
+    actual = shelter.dequeue('lion')
+    expected = None
     assert actual == expected
